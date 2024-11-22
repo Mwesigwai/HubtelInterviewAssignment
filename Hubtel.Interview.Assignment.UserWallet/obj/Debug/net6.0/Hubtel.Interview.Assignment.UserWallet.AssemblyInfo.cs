142cde7ab5b44ed57d1c1f0427abd4ad43ed4960
isaacMwesigwa <mwesigwai433@gmail.com>
2024-11-22 16:51:21 +0300

    commit4

--- Hubtel.Interview.Assignment.UserWallet/obj/Debug/net6.0/Hubtel.Interview.Assignment.UserWallet.AssemblyInfo.cs
+++ Hubtel.Interview.Assignment.UserWallet/obj/Debug/net6.0/Hubtel.Interview.Assignment.UserWallet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hubtel.Interview.Assignment.UserWallet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedf9db76defa0d018fb3a3c46bd53aa01ee0701")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ccfbb2f810c14962309bbc4ad325c7d24ae1f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hubtel.Interview.Assignment.UserWallet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hubtel.Interview.Assignment.UserWallet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
